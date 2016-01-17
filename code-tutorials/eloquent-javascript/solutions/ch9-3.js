0e142a447d683bd90532c1fe9dfd280cea5aa10d
sam <smrgrace@gmail.com>
2016-01-17 09:51:09 +0000

    remove tutorial code

--- code-tutorials/eloquent-javascript/solutions/ch9-3.js
+++
@@ -1,4 +0,0 @@
-// my solution. similar but a bit different to official answer.
-// for the first bit it uses (\+|-|) instead of [+-]?
-
-var number = /^[-+]?((\d+\.?)|(\.?\d+))\d*([eE][+-]?\d+)?$/;
